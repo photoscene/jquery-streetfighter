88f0e23a39a872400956dbd204e6749f880e697c
Csaba Fazekas <csabafazekasster@gmail.com>
2014-05-26 15:33:21 +0200

    final steps

--- js/app.js
+++ js/app.js
@@ -25,10 +25,34 @@ $(document).ready(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
+  
+  $('html').keydown(function(event) {
+      if (event.keyCode === 88) {
+        playcool();
+        $('.ryu-action').hide();
+        $('.ryu-still').hide();
+        $('.ryu-cool').show();
+    }
+  })
+    .keyup(function(event) {
+        // topcool();
+        $('#cool-sound')[0].pause();
+        $('#cool-sound')[0].load();
+        if (event.keyCode === 88) {
+        $('.ryu-cool').hide();
+        $('.ryu-still').show();
+    }
+  });
 });
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
+}
+
+function playcool () {
+  $('#cool-sound')[0].volume = 0.5;
+  $('#cool-sound')[0].load();
+  $('#cool-sound')[0].play();
 }
